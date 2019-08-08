a2697fea86964dd7c4420aa7e9b3d6e15f7d372b
Max Zamkovoy <max.zamkovoy@gmail.com>
2019-08-08 22:06:29 +0300

    Added check for add to cart button

--- src/main/java/com/amazon/pages/ProductDetailsPage.java
+++ src/main/java/com/amazon/pages/ProductDetailsPage.java
@@ -1,10 +1,13 @@
 package com.amazon.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 public class ProductDetailsPage {
 
     private final String PRODUCT_DETAIL_URL = "https://www.amazon.com/Apple-iPhone-Fully-Unlocked-128/dp/B07P976BBH/";
+    private final By ADD_TO_CART_BUTTON = By.id("add-to-cart-button");
 
     private final WebDriver DRIVER;
 
@@ -14,6 +17,11 @@ public class ProductDetailsPage {
     }
 
     public boolean isAddToCartButtonExist() {
-        return true;
+        try {
+            DRIVER.findElement(ADD_TO_CART_BUTTON);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
 }
